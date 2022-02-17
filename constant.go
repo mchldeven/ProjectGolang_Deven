52f25b53c836c9f472ca35646b8fc3363d0068d1
michael.deven1999@gmail.com <97597835+michaeldeven@users.noreply.github.com>
2022-02-17 14:14:29 +0700

    const cuy

---
+++ constant.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	const (
+		namaDepan string  	="Kezia"
+		namabelakang 		="Milennia"
+		namalengkap		    ="Kezia milennia"
+		value 				=10000
+	)
+	fmt.Println(namaDepan)
+	fmt.Println(namabelakang)
+	fmt.Println(namalengkap)
+	fmt.Println(value)
+}
