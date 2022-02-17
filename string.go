911f80df627f933845e2c6b67855cf8d5174a48e
michael.deven1999@gmail.com <97597835+michaeldeven@users.noreply.github.com>
2022-02-17 11:58:57 +0700

    penambahan string

---
+++ string.go
@@ -0,0 +1,19 @@
+// string adalah karakter 0-tak terhinggga
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Michael Deven Bahari")
+	fmt.Println("Kezia Milennia Tanod")
+	fmt.Println("Eveline Mary")
+
+	fmt.Println(len("MichaelDevenBahari"))
+	fmt.Println("Kezia Milennia Tanod"[0])
+	fmt.Println("Eveline Mary"[1])
+}
+
+
+//string pake "karakternya"
+//spasi dihitung 1 
