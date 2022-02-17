5b587fe4a7a1b933bf4851e4231ca0df4e5ab44f
michael.deven1999@gmail.com <97597835+michaeldeven@users.noreply.github.com>
2022-02-17 12:32:18 +0700

    nambah variable

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	nama := ("MichaelDevenBahari")
+	fmt.Println(nama)
+
+	var jabatan string
+	jabatan = "ITprogammer"
+	fmt.Println(jabatan)
+
+	var divisi = "Digital Banking"
+	fmt.Println(divisi)
+
+
+}
