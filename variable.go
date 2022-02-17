768d13014a4c2242b43c45de4b62b8bc49233838
michael.deven1999@gmail.com <97597835+michaeldeven@users.noreply.github.com>
2022-02-17 14:32:02 +0700

    perubahan variable

--- variable.go
+++ variable.go
@@ -13,5 +13,12 @@ func main(){
 	var divisi = "Digital Banking"
 	fmt.Println(divisi)
 
+	var (
+		namaDepan = "Michael"
+		namabelakang ="Deven"
+		
+	)
+	fmt.Println(namaDepan)
+	fmt.Println(namabelakang)
 
 }
