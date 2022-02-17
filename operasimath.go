f6cacff8b5ba323ba6ee6a9d55885b1398717874
michael.deven1999@gmail.com <97597835+michaeldeven@users.noreply.github.com>
2022-02-17 16:04:48 +0700

    ngitung operasi math

---
+++ operasimath.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x = 100
+	var y = 6
+	var c = x*y
+	fmt.Println(c)
+
+	var deven = 1
+	var kezia = 9
+	var keuntungan = deven-kezia
+	fmt.Println(keuntungan)
+}
